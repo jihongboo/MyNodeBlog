be613ca0638755ffcb41dbcf38ccff87394efb6a
纪洪波 <jihongboo@qq.com>
2018-01-30 00:01:51 +0800

    init project

---
+++ build/app.js
@@ -0,0 +1,14 @@
+"use strict"
+Object.defineProperty(exports, "__esModule", {value: true})
+const Koa = require("koa")
+let app = new Koa()
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+    await next()
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>'
+})
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...')
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import * as Koa from "koa"
+
+let app = new Koa()
+app.use(async (ctx, next) => {
+    await next()
+    ctx.response.type = 'text/html'
+    ctx.response.body = '<h1>Hello, koa2!</h1>'
+})
+app.listen(3000)
+console.log('app started at port 3000...')
